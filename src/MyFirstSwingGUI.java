70d493d8089d2a0e72bc6930a28c5be51bd5fe7d
IzzyR6 <League@administorsimac.attlocal.net>
2018-06-24 17:22:58 -0700

    swing class, photo quiz

--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -15,31 +15,31 @@ import javax.swing.JPanel;
 public class MyFirstSwingGUI {
 	public static void main(String[] args) {
 		//1. Create and initialize an object of the JFrame class
-
+JFrame a = new JFrame();
 		//2. Set your JFrame object to be visible
-
+a.setVisible(true);
 		//3. Run your program. Do you see your window? It's probably very small.
 		
 		//4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
+a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//5. Create and initialize an object of the JPanel class
-
+JPanel b = new JPanel();
 		//6. Create and initialize an object of the JLabel class
-
+JLabel c = new JLabel();
 		//7. Set the text of the JLabel to a lovely greeting.
-
+c.setText("Hello lovely.");
 		//8. Add the JPanel object to the JFrame
-
+a.add(b);
 		//9. Add the JLabel object to the JPanel
-
+b.add(c);
 		//10. Pack your JFrame.
-
+a.pack();
 		//11. Run your program again. Do you see your message.
 		
 		//12. Use the loadImage method to set the icon of the JLabel object.  
-
+c.setIcon(loadImage());
 		//13. Re-pack the JFrame object.
-		
+		a.pack();
 		//14. Run the program one more time. Do you see the image?
 	}
 	
--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -3,7 +3,6 @@
  *    Level 1
  */
 
-
 import java.awt.Component;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -19,36 +18,51 @@ public class PhotoQuiz {
 	public static void main(String[] args) throws Exception {
 		JFrame quizWindow = new JFrame();
 		quizWindow.setVisible(true);
-                quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
+		quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // This will make sure the program exits when you
+																	// close the window
 
-		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+		// 1. find an image on the internet, and put its URL in a String variable (from
+		// your browser, right click on the image, and select “Copy Image Address”)
+		String claire = "https://pbs.twimg.com/media/DB1oD1jVwAAJvBH.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component a;
 		// 3. use the "createImage()" method below to initialize your Component
-
+		a = createImage(claire);
 		// 4. add the image to the quiz window
-
+		quizWindow.add(a);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String b = JOptionPane.showInputDialog("How many heads are there?");
+		int bn = Integer.parseInt(b);
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if (b.equals(12)) {
+			JOptionPane.showMessageDialog(null, "correct");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
-
-		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+		else {
+			JOptionPane.showMessageDialog(null, "Incorrect");
+		}
+		// 9. remove the component from the quiz window (you may not see the effect of
+		// this until step 12)
+		quizWindow.remove(a);
 		// 10. find another image and create it (might take more than one line of code)
-
+		String ugly = "https://pbs.twimg.com/profile_images/935756401166741505/pnhb9KSg.jpg";
+		Component c;
+		c = createImage(ugly);
 		// 11. add the second image to the quiz window
-
+		quizWindow.add(c);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-
+		String d = JOptionPane.showInputDialog("On a scale of 1-10, how ugly is Claire?");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+		if (d.equals("10")) {
+			JOptionPane.showMessageDialog(null, "Correct");
+		} else {
+			JOptionPane.showMessageDialog(null, "WRONG");
+			JOptionPane.showMessageDialog(null, "The one of the left is claire");
+		}
 	}
 
 	private static Component createImage(String imageUrl) throws MalformedURLException {
@@ -60,10 +74,6 @@ public class PhotoQuiz {
 
 	/* OPTIONAL */
 	// *14. add scoring to your quiz
-	// *15. make something happen when mouse enters image (imageComponent.addMouseMotionListener()) 
+	// *15. make something happen when mouse enters image
+	// (imageComponent.addMouseMotionListener())
 }
-
-
-
-
-
